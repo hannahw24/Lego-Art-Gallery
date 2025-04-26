d4bcc8f8c376bbf05f61f6fa8d43363450dcb272
Hannah Wong <hannah@Hannahs-MacBook-Pro-5.local>
2025-04-26 16:23:47 -0400

    Getting image page set

--- scripts/gallery.js
+++ scripts/gallery.js
@@ -9,15 +9,15 @@
 // 			(el.style.display = 'flex')
 // 		);
 // };
-document.addEventListener('DOMContentLoaded', (event) => {
-	const colors = ['#c91a09', '#ffd700', '#0055bf'];
-	document
-		.querySelectorAll('.frame')
-		.forEach(
-			(el) => (
-				(el.style.backgroundColor =
-					colors[Math.floor(Math.random() * colors.length)]),
-				(el.style.display = 'flex')
-			)
-		);
-});
+// document.addEventListener('DOMContentLoaded', (event) => {
+// 	const colors = ['#c91a09', '#ffd700', '#0055bf'];
+// 	document
+// 		.querySelectorAll('.frame')
+// 		.forEach(
+// 			(el) => (
+// 				(el.style.backgroundColor =
+// 					colors[Math.floor(Math.random() * colors.length)]),
+// 				(el.style.display = 'flex')
+// 			)
+// 		);
+// });
